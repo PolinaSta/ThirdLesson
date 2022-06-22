e599b5ba72500cf7dcdab209a31293ce238f3248
Polina <polstaver@yandex.by>
2022-06-22 13:48:46 +0300

    Сохранение семинара

--- Program.cs
+++ Program.cs
@@ -20,6 +20,29 @@
 // getDot(x1, y1);
 
 // 2. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+void getDot (int num)
+{
+if (num == 1)
+{
+Console.WriteLine("x>0 , y>0");
+}
+else if(num == 2){
+Console.WriteLine("x<0 , y>0");
+}
+else if(num == 3){
+Console.WriteLine("x<0 , y<0");
+}
+else if(num == 4){
+Console.WriteLine("x>0 , y<0");
+}
+else{
+   Console.WriteLine("Задайте другой номер"); 
+}
+} 
+Console.WriteLine("Введите точку: ");
+int num = Convert.ToInt32(Console.ReadLine());
+getDot(num);
+
 
 // 3. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 // Console.Clear();
